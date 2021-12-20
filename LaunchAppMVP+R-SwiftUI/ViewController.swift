00793015a01fce5ca6a4c4f94d834157c59e2a30
Anton Vlezko <vlezko.anton@yandex.ru>
2021-12-20 17:48:19 +0300

    Initial Commit

---
+++ LaunchAppMVP+R-SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchAppMVP+R-SwiftUI
+//
+//  Created by Anton Vlezko on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
